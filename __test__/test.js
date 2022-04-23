a40a8213a25863890365769ac651fb879ff0bd4a
Frank Mayer <frank.mayer1907@protonmail.com>
2022-04-23 19:23:09 +0200

    added tests

---
+++ __test__/test.js
@@ -0,0 +1,7 @@
+import { yahp } from "..";
+import fs from "fs";
+
+const input = fs.readFileSync("./__test__/test.html", "utf8");
+yahp(input).then((output) => {
+  fs.writeFileSync("./__test__/output.html", output);
+});
